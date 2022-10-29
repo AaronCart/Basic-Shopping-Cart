ff2674fe60d2d39fc33f83b6467a57af9bdafca4
AaronCart <115600461+AaronCart@users.noreply.github.com>
2022-10-29 16:09:55 +1100

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-sm bg-info navbar-dark">
+    <ul class="navbar-nav big-font mr-auto mx-4 pl-4">
+        <li class="nav-item pr-4">
+            <a class="nav-link text-light bg-info">Payment Gateway</a>
+        </li>
+        <li class="nav-item pr-4">
+            <a class="nav-link text-light bg-info" href="home.php">Home</a>
+        </li>
+    </ul>
+    <ul class="navbar-nav big-font">
+        <li class="nav-item mx-4 pr-4">
+            <a class="nav-link text-light bg-info" href="client.php">Generate Session Key</a>
+        </li>
+        <li class="nav-item mx-4 pr-4">
+            <a class="nav-link text-light bg-info" href="shopping_cart.php">Cart</a>
+        </li>
+        <li class="nav-item mx-4 pr-4">
+            <a class="nav-link text-light bg-info" href="logout.php">Log Out</a>
+        </li>
+    </ul>
+</nav>
